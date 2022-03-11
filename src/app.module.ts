4aa7a8258a4d66ed5589fb50235e981e970ad3c3
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-03-12 00:35:40 +0900

    delete: controllers,providers in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { typeORMConfig } from './configs/typeorm.config';
-import { CrawlingController } from './crawling/crawling.controller';
 import { CrawlingModule } from './crawling/crawling.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), CrawlingModule],
-  controllers: [AppController, CrawlingController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
