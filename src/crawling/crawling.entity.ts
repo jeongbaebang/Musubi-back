9756c696c25c8d897b04daa0863e0180371f3ce2
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-03-12 05:20:37 +0900

    update: crawling.entity.ts

--- src/crawling/crawling.entity.ts
+++ src/crawling/crawling.entity.ts
@@ -1,7 +1,16 @@
-import { Entity, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, BaseEntity, Column } from 'typeorm';
 
 @Entity()
 export class Crawling extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  uuid: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
 }
