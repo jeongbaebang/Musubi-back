de7a5468d34e0d0c120bd5ce778cc7b06ab5dd06
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-03-12 00:27:46 +0900

    update: add TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeORMConfig } from './configs/typeorm.config';
 import { CrawlingController } from './crawling/crawling.controller';
 import { CrawlingModule } from './crawling/crawling.module';
 
 @Module({
-  imports: [CrawlingModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), CrawlingModule],
   controllers: [AppController, CrawlingController],
   providers: [AppService],
 })
