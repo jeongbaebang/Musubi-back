544a682bd5c6ea5ae54579ac3201a1ac5a46e600
blackzapato <rhkrrldudsla@gmail.com>
2022-03-11 21:26:48 +0900

    update: 크롤링 관련 소스

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('/start')
+  getCrawling() {
+    return this.appService.getCrawling();
+  }
 }
