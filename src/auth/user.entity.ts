c31cde9b3df8ab0a165346be8d3d6256d1c617ec
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-04-03 00:19:00 +0900

    create: Closed #7 user를 위한 Entity 생성

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,26 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'TMI_USER_M' })
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn({ name: 'N_USER_NO' })
+  id: number;
+
+  @CreateDateColumn({ name: 'D_REG_DTM' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'D_MOD_DTM' })
+  updatedAt: Date;
+
+  @Column({ name: 'V_EMAIL' })
+  username: string;
+
+  @Column({ name: 'V_PASSWORD' })
+  password: string;
+}
