7c31b84ecfa1518ec54775913273a5e61bbab861
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-04-05 18:01:13 +0900

    create: 애플리케이션에 PassPort 모듈 등록 #30

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthController } from './auth.controller';
@@ -8,11 +9,14 @@ import { UserRepository } from './user.repository';
 
 @Module({
   imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
     JwtModule.register({
       //TODO: 배포시 .env 파일에 보관할것
       secret: 'root',
       signOptions: {
-        expiresIn: '1day',
+        expiresIn: '1d',
       },
     }),
     TypeOrmModule.forFeature([UserRepository]),
