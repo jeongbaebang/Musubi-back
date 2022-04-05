50502e7d3637ff85e62cfff9dee9215a834ba37d
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-04-05 17:59:20 +0900

    create: 애플리케이션에 JWT 모듈 등록 #30

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthController } from './auth.controller';
@@ -6,7 +7,16 @@ import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    JwtModule.register({
+      //TODO: 배포시 .env 파일에 보관할것
+      secret: 'root',
+      signOptions: {
+        expiresIn: '1day',
+      },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
